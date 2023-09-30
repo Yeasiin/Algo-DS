606f5d839ff796ef60b89a06b1bc892e02096f1d
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2023-09-30 18:33:08 +0600

    basic code run

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world again? how are you shit this now work");
+
+    int a = 10;
+
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("\nhello world %d", i);
+    }
+
+    return 0;
+}
